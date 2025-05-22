267cc6ea75c92c84c38f2c06760704175ac9934c
Jonathan Mui <jonathanmui4@gmail.com>
2025-05-22 20:06:18 +0800

    Create dummy data and settle data type format

--- recipe-vault-frontend/src/types/Recipe.ts
+++ recipe-vault-frontend/src/types/Recipe.ts
@@ -1,8 +1,33 @@
+export type Ingredient =
+  | {
+      id?: number;
+      ingredientName: string;
+      amount?: string;
+    }
+  | string;
+
 export interface Recipe {
-  id: number;
+  id?: number;
   title: string;
-  difficultyLevel: 'Easy' | 'Medium' | 'Hard';
-  ingredientsCount: number;
+  difficulty: 'EASY' | 'MEDIUM' | 'HARD';
+  ingredients?: Ingredient[];
+  ingredientCount?: number;
+  instructions?: string;
+  imageUrl?: string;
   creatorName: string;
-  creationDate: string;
+  createdDate: string;
 }
+
+export interface RecipeFormData {
+  title: string;
+  difficulty: 'EASY' | 'MEDIUM' | 'HARD';
+  ingredients: Ingredient[];
+  instructions: string;
+  imageUrl: string;
+}
+
+export const DifficultyLevels = {
+  EASY: 'Easy' as const,
+  MEDIUM: 'Medium' as const,
+  HARD: 'Hard' as const,
+};
