508f1b1daa0f23c7786ea3ef666079fcf696f64a
Jonathan Mui <jonathanmui4@gmail.com>
2025-05-24 17:16:53 +0800

    Fix nginx script for frontend deploy

--- recipe-vault-frontend/start-nginx.sh
+++ recipe-vault-frontend/start-nginx.sh
@@ -3,7 +3,8 @@
 # Check if we're on Railway (Railway sets RAILWAY_ENVIRONMENT)
 if [ -n "$RAILWAY_ENVIRONMENT" ]; then
     echo "Detected Railway environment, using railway nginx config"
-    cp /etc/nginx/conf.d/railway.conf /etc/nginx/conf.d/default.conf
+    # Use envsubst to replace environment variables in the config
+    envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/railway.conf > /etc/nginx/conf.d/default.conf
 else
     echo "Detected Docker Compose environment, using docker nginx config"
     cp /etc/nginx/conf.d/docker.conf /etc/nginx/conf.d/default.conf
